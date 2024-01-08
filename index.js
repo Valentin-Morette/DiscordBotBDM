dae3ac491bf0e443b4c2c3481f84c4ce53d73dfb
valentin-morette <morette.valentin@gmail.com>
2024-01-08 19:45:39 +0100

    add funny emoji after bot response

--- index.js
+++ index.js
@@ -1,55 +1,52 @@
-const { Client, GatewayIntentBits } = require('discord.js');
+const { Client, GatewayIntentBits, bold, italic, time } = require('discord.js');
 const dotenv = require('dotenv');
 
 dotenv.config();
 
 function removePunctuationAndTrailingSpaces(inputString) {
-  return inputString.replace(/[,?!.:;]|\s+$/g, '');
+	return inputString.replace(/[,?!.:;]|\s+$/g, '');
 }
 
 const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent,
-  ],
+	intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent],
 });
 
 client.on('ready', () => {
-  console.log('Ready!');
+	console.log('Bot BDM Ready!');
 });
 
-// Le bot écoute les messages sur le serveur
 client.on('messageCreate', (message) => {
-  const com = removePunctuationAndTrailingSpaces(message.content);
-  const arrCom = [
-    'ping',
-    'quoi',
-    'dis',
-    'dit',
-    'hein',
-    "c'est ou",
-    'oui',
-    'ouais',
-    'si',
-  ];
-  const arrRep = [
-    'pong !',
-    '...feur',
-    '...nosaure',
-    '...recteur',
-    'deux !',
-    'Dans ton cul !',
-    '...stiti',
-    '...stern',
-    '...lence',
-  ];
+	const com = removePunctuationAndTrailingSpaces(message.content);
+	const arrCom = ['ping', 'quoi', 'dis', 'dit', 'hein', "c'est ou", 'oui', 'ouais', 'si'];
+	const arrRep = [
+		'pong !',
+		'...feur',
+		'...nosaure',
+		'...recteur',
+		'deux !',
+		'Dans ton cul !',
+		'...stiti',
+		'...stern',
+		'...lence',
+	];
+	const arrEmojis = ['😂', '🤣 ', '😜', '🤪', '🤡', '😹', '🙃', '🤖', '🤭'];
 
-  arrCom.map((x) => {
-    if (com.endsWith(x)) {
-      message.channel.send(arrRep[arrCom.indexOf(x)]);
-    }
-  });
+	arrCom.map((x) => {
+		if (com.endsWith(x)) {
+			message.channel.send(
+				arrRep[arrCom.indexOf(x)] + ' ' + arrEmojis[Math.floor(Math.random() * arrEmojis.length)]
+			);
+		}
+	});
+});
+
+client.on('messageDelete', (message) => {
+	message.channel.send(`Message supprimé par ${message.author.username} : ${message.content}`);
+});
+
+client.on('channelCreate', (channel) => {
+	console.log(channel);
+	channel.send('Nouveau channel créé !');
 });
 
 client.login(process.env.TOKEN);
